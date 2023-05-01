8530794e25274be15b1bd8e563e0b1754d5cf505
Hextechagon <mwang0401@gmail.com>
2023-05-01 00:08:41 -0400

    implemented cogs

--- Bot/bot.py
+++ Bot/bot.py
@@ -16,8 +16,8 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-from coach_helpers import insert_coach, bulk_insert, delete_coach, replace_coach, get_leaderboard
-from draft_helpers import *
+from cogs.coach import Coach
+from cogs.draft import Draft
 
 
 load_dotenv()
@@ -27,100 +27,18 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-draft_order = []
 
 @bot.event
 async def on_ready():
     """Display a connection message."""
     print(f'{bot.user.name} has connected to Discord!')
+    await bot.add_cog(Coach(bot))
+    await bot.add_cog(Draft(bot))
 
 @bot.command()
 async def guide(ctx):
     """Display a list of all commands."""
     await ctx.send('Help message here!')
-
-@bot.command()
-@commands.has_role('Draft Host')
-@commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
-async def register(ctx, user: discord.Member, team_name = 'TBD'):
-    """Enter the specified server member into the draft league."""
-    # code to ping user await ctx.send(f'Hello <@{user.id}>!')
-    status = insert_coach(user.id, user.name, team_name)
-    if status == 0:
-        await ctx.send(f':ballot_box_with_check: {user.name} has been registered as a coach.')
-    elif status == 1:
-        await ctx.send(':x: The draft league is currently full.')
-    else:
-        await ctx.send(f':x: {user.name} is already a coach.')
-
-@bot.command()
-@commands.has_role('Draft Host')
-@commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
-async def bulk_register(ctx, *args: discord.Member):
-    """Enter the specified server members into the draft league."""
-    # TODO: status = bulk_insert(args)
-
-@bot.command()
-@commands.has_role('Draft Host')
-@commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
-async def delete(ctx, user: discord.Member):
-    """Remove the specified server member from the draft league."""
-    status = delete_coach(user.id)
-    if status == 0:
-        await ctx.send(f':ballot_box_with_check: {user.name} has been removed as a coach.')
-    else:
-        await ctx.send(f':x: {user.name} is not a valid coach.')
-
-@bot.command()
-@commands.has_role('Draft Host')
-@commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
-async def replace(ctx, user1: discord.Member, user2: discord.Member, team_name = 'TBD'):
-    """Replace a current coach with the specified server member (inherits all previous coach data)."""
-    status = replace_coach(user1.id, user2.id, user2.name, team_name)
-    if status == 0:
-        await ctx.send(f':ballot_box_with_check: {user1.name} has been replaced by {user2.name} as a coach.')
-    elif status == 1:
-        await ctx.send(f':x: {user1.name} is not a valid coach.')
-    else:
-        await ctx.send(f':x: {user2.name} is already a coach.')
-
-@register.error
-@delete.error
-@replace.error
-async def error_handler(ctx, error):
-    """Check if the entered user is a server member."""
-    if isinstance(error, commands.MissingRole):
-        await ctx.send(':x: You do not have permission to use this command.')
-    elif isinstance(error, commands.errors.MemberNotFound):
-        await ctx.send(':x: Please specify valid server member(s).')
-
-@bot.command()
-async def rank(ctx):
-    """Display the leaderboard containing all current coaches."""
-    coaches = get_leaderboard()
-    output = '```' + '***Leaderboard***\n' + coaches + '```'
-    await ctx.send(output)
-
-@bot.command()
-async def start(ctx):
-    """Initiate the draft process by randomizing the draft order and beginning the timer."""
-    # TODO
-
-@bot.command()
-async def select(ctx, pokemon):
-    """Add the specified pokemon to the coach's party."""
-    # TODO
-
-@bot.command()
-async def add(ctx, *pokemon):
-    """Add the specified pokemon(s) to the coach's party."""
-    # TODO: see comment above (ONLY available after drafting finished)
-    # recommend do remove before add to make sure budget not exceeded
-
-@bot.command()
-async def remove(ctx, *pokemon):
-    """Remove the specified pokemon(s) from the coach's party."""
-    # TODO: see comment above (ONLY available after drafting finished)
 
 
 bot.run(TOKEN)
---
+++ Bot/cogs/coach.py
@@ -0,0 +1,74 @@
+"""Commands for coaches."""
+import discord
+from discord.ext import commands
+from coach_helpers import insert_coach, delete_coach, replace_coach, get_leaderboard
+
+
+class Coach(commands.Cog):
+    """Coaches class."""
+
+    def __init__(self, bot):
+        """Initialize the coach cog."""
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_role('Draft Host')
+    @commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
+    async def register(self, ctx, user: discord.Member, team_name = 'TBD'):
+        """Enter the specified server member into the draft league."""
+        # code to ping user await ctx.send(f'Hello <@{user.id}>!')
+        status = insert_coach(user.id, user.name, team_name)
+        if status == 0:
+            await ctx.send(f':ballot_box_with_check: {user.name} has been registered as a coach.')
+        elif status == 1:
+            await ctx.send(':x: The draft league is currently full.')
+        else:
+            await ctx.send(f':x: {user.name} is already a coach.')
+
+    @commands.command()
+    @commands.has_role('Draft Host')
+    @commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
+    async def bulk_register(self, ctx, *args: discord.Member):
+        """Enter the specified server members into the draft league."""
+        # TODO: status = bulk_insert(args)
+
+    @commands.command()
+    @commands.has_role('Draft Host')
+    @commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
+    async def delete(self, ctx, user: discord.Member):
+        """Remove the specified server member from the draft league."""
+        status = delete_coach(user.id)
+        if status == 0:
+            await ctx.send(f':ballot_box_with_check: {user.name} has been removed as a coach.')
+        else:
+            await ctx.send(f':x: {user.name} is not a valid coach.')
+
+    @commands.command()
+    @commands.has_role('Draft Host')
+    @commands.check(lambda ctx: ctx.channel.id == 1085977763833446401)
+    async def replace(self, ctx, user1: discord.Member, user2: discord.Member, team_name = 'TBD'):
+        """Replace a current coach with the specified server member (inherits all previous coach data)."""
+        status = replace_coach(user1.id, user2.id, user2.name, team_name)
+        if status == 0:
+            await ctx.send(f':ballot_box_with_check: {user1.name} has been replaced by {user2.name} as a coach.')
+        elif status == 1:
+            await ctx.send(f':x: {user1.name} is not a valid coach.')
+        else:
+            await ctx.send(f':x: {user2.name} is already a coach.')
+
+    @commands.command()
+    async def rank(self, ctx):
+        """Display the leaderboard containing all current coaches."""
+        coaches = get_leaderboard()
+        output = '```' + '***Leaderboard***\n' + coaches + '```'
+        await ctx.send(output)
+   
+    @register.error
+    @delete.error
+    @replace.error
+    async def error_handler(self, ctx, error):
+        """Check if the entered user is a server member."""
+        if isinstance(error, commands.MissingRole):
+            await ctx.send(':x: You do not have permission to use this command.')
+        elif isinstance(error, commands.errors.MemberNotFound):
+            await ctx.send(':x: Please specify valid server member(s).')
